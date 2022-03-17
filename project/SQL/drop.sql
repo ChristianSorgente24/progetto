c1c2f49094acc4675364c054a9c733194350a345
utente <utente@LAPTOP-987TL91F.station>
2022-03-17 17:50:36 +0100

    drop by chri

---
+++ project/SQL/drop.sql
@@ -0,0 +1,7 @@
+drop table corso cascade constraint;
+drop table corsista cascade constraint;
+drop table docente cascade constraint;
+drop table amministratore cascade constraint;
+drop table corsocorsista cascade constraint;
+drop sequence CodGeneratorCorso_seq;
+drop sequence CodGeneratorCorsista_seq;
