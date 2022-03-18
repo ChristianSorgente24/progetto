488645fd6293723816c37204c058106fbe09e1d2
39331 <39331@LAPTOP-VD7E07NT>
2022-03-18 14:26:06 +0100

    DocenteBC

---
+++ project/src/main/java/com/milano/businesscomponent/DocenteBC.java
@@ -0,0 +1,25 @@
+package com.milano.businesscomponent;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import com.milano.architecture.dbaccess.DBAccess;
+import com.milano.businesscomponent.model.Docente;
+import com.milano.architecture.dao.*;
+
+public class DocenteBC {
+
+		private  Connection conn;
+	
+	public DocenteBC() throws ClassNotFoundException, DAOException, IOException {
+		conn = DBAccess.getConnection();
+	}
+	public Docente getByCod(long codDocente) throws DAOException{
+		try {	
+		return DocenteDAO.getFactory().getByCod(conn, codDocente);
+	}catch(SQLException sql) {
+		throw new DAOException(sql);
+	}
+	}
+}	
