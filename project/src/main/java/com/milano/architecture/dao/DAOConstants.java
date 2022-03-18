e6b92071ff2975ea4a9385998ff17b35e0bbb7bb
Lavoro <Lavoro@DESKTOP-9D5K1D2.homenet.telecomitalia.it>
2022-03-18 10:40:44 +0100

    DAOCostants - Query aggiornate

--- project/src/main/java/com/milano/architecture/dao/DAOConstants.java
+++ project/src/main/java/com/milano/architecture/dao/DAOConstants.java
@@ -1,23 +1,34 @@
 package com.milano.architecture.dao;
 
 public interface DAOConstants {
-	String SELECT_DOCENTE_BYCOD = "select * from docente where codDocente = ?";
+	//Select codgenerator
 	String SELECT_CORSO_SEQ = "Select CodGeneratorCorso_seq.nextval from dual";
 	String SELECT_CORSISTA_SEQ = "Select CodGeneratorCorsista_seq.nextval from dual";
 	
+	//CRUD corsista
 	String SELECT_CORSISTA = "Select * from corsista";
 	String UPDATE_CORSISTA = "Update corsista set nomeCorsista = ?, cognomeCorsista = ?, precedentiFormativi = ? where codCorsista = ?";
 	String DELETE_CORSISTA = "Delete from corsista where codCorsista = ?";
 	
+	//CRUD docente
 	String SELECT_DOCENTE = "Select * from docente";
-
-	String SELECT_CORSOCORSISTA = "Select * from corsocorsista";
-	String UPDATE_CORSOCORSISTA = "Update corsocorsista set codCorso = ?, codCorsista = ?";
-	String DELETE_CORSOCORSISTA = "Delete from corsista where codCorso = ?, codCorsista = ?";
-	String SELECT_CORSOCORSISTA_BYCOD = "select * from corsista where codCorsista = ?";
+	String SELECT_DOCENTE_BYCOD = "select * from docente where codDocente = ?";
 	
+	//CRUD corsocorsista
+	String SELECT_CORSOCORSISTA = "Select * from corsocorsista";
+	String UPDATE_CORSOCORSISTA = "Update corsocorsista set codCorso = ? AND codCorsista = ?";
+	String DELETE_CORSOCORSISTA = "Delete from corsocorsista where codCorso = ? AND codCorsista = ?";
+	String SELECT_CORSOCORSISTA_BYCOD = "select * from corsocorsista where codCorso = ? AND codCorsista = ?";
+	
+	//CRUD corso
 	String SELECT_CORSO = "select * from corso";
 	String UPDATE_CORSO = "update corso set nomeCorso = ?, dataInizioCorso = ?, dataFineCorso = ?, costoCorso = ?, commentiCorso = ?, aulaCorso = ?, codDocente = ? where codCorso = ?";
 	String DELETE_CORSO = "delete from corso where codCorso = ?";
 	String SELECT_CORSO_BYCOD = "select * from corso where codCorso = ?";
+
+	//login
+	String SELECT_ADMINPASS = "Select codAdmin from amministratore where username = ?";
+	
+	//statistiche
+	String SELECT_CORSO_RECENTE = "Select MIN(dataInizioCorso) from corso";
 }
