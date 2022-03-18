2c8048f020c239e45b2280ebaa6b16c27a8f876f
punta <punta@192.168.178.53>
2022-03-18 14:50:17 +0100

    prime due stats Utitlity

--- project/src/main/java/com/milano/businesscomponent/utility/StatsUtility.java
+++ project/src/main/java/com/milano/businesscomponent/utility/StatsUtility.java
@@ -1,5 +1,21 @@
 package com.milano.businesscomponent.utility;
 
+import java.io.IOException;
+
+import com.milano.architecture.dao.DAOException;
+import com.milano.businesscomponent.CorsistaBC;
+import com.milano.businesscomponent.model.Corsista;
+
 public class StatsUtility {
+
+	public static int numeroCorsistiTotali() throws ClassNotFoundException, DAOException, IOException {
+		CorsistaBC cBC = new CorsistaBC();
+		return cBC.getAll().length;	
+	}
+	
+	public static Corsista[] elencoCorsisti() throws ClassNotFoundException, DAOException, IOException {
+		CorsistaBC cBC = new CorsistaBC();
+		return cBC.getAll();
+	}
 	
 }
