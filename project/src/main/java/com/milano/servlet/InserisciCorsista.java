d51a6ec1e3d005815b3a0c23c4cf879e131ca15e
39331 <39331@LAPTOP-VD7E07NT>
2022-03-21 15:29:36 +0100

    InserisciCorsista Servlet

---
+++ project/src/main/java/com/milano/servlet/InserisciCorsista.java
@@ -0,0 +1,41 @@
+package com.milano.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.milano.architecture.dao.DAOException;
+import com.milano.businesscomponent.CorsistaBC;
+import com.milano.businesscomponent.model.Corsista;
+
+
+@WebServlet("/InserisciCorsista")
+public class InserisciCorsista extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		Corsista corsisti = new Corsista();
+		
+		corsisti.setNomeCorsista(request.getParameter("nomeCorsista"));
+		corsisti.setCognomeCorsista(request.getParameter("cognomeCorsista"));
+		corsisti.setCodCorsista(Long.parseLong(request.getParameter("codCorsista")));
+		corsisti.setPrecedentiFormativi(Byte.parseByte(request.getParameter("PrecendentiFormativi")));
+		
+		response.sendRedirect("index.jsp");
+		
+		try {
+			CorsistaBC cBC = new CorsistaBC();
+			cBC.createCorsista(corsisti);
+		} catch (DAOException | ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+			throw new ServletException(e);
+		}
+	}
+
+	}
