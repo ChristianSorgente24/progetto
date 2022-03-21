11fe314239a2b1afe237091b094500ec204dc839
utente <utente@LAPTOP-987TL91F.homenet.telecomitalia.it>
2022-03-21 12:40:08 +0100

    tolto import inutili

--- project/src/main/java/test/com/milano/architecture/dao/DocenteDAOTest.java
+++ project/src/main/java/test/com/milano/architecture/dao/DocenteDAOTest.java
@@ -1,6 +1,5 @@
 package test.com.milano.architecture.dao;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.io.IOException;
@@ -11,11 +10,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
-import com.milano.architecture.dao.CorsoDAO;
 import com.milano.architecture.dao.DAOException;
 import com.milano.architecture.dao.DocenteDAO;
 import com.milano.architecture.dbaccess.DBAccess;
-import com.milano.businesscomponent.model.Corso;
 import com.milano.businesscomponent.model.Docente;
 
 class DocenteDAOTest {
